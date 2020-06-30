be0017f5d2a080f49248bbbeda031a0b3bfe6905
zj <54255578+cszjing@users.noreply.github.com>
2020-06-30 11:29:10 +0800

    Add files via upload
    出现问题，AttributeError: 'module' object has no attribute 'histogram_summary'，照下边这篇博客修改就行
    https://blog.csdn.net/chencaw/article/details/78557903

--- add_layer.py
+++ add_layer.py
@@ -3,18 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    with tf.name_scope('layer'):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    layer_name = 'layer%s'%n_layer
+    with tf.name_scope(layer_name):
         with tf.name_scope('weight'):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            tf.summary.histogram(layer_name + '/weights', Weights)
         with tf.name_scope('bias'):
             bias = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.summary.histogram(layer_name + '/bias', bias)
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), bias)
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b)
+        tf.summary.histogram(layer_name + '/outputs', outputs)
         return outputs
 
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
@@ -24,17 +28,19 @@ y_data = np.square(x_data) - 0.5 + noise
 with tf.name_scope('inputs'):
     xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-prediction = add_layer(l1, 10, 1, activation_function=None)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                                         reduction_indices=[1]))
+    tf.summary.scalar('loss', loss)
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter('logs/', sess.graph)  # 定义好sess后，定义writer
 sess.run(init)
 
@@ -47,6 +53,9 @@ plt.show()
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
+        result = sess.run(merged,
+                          feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result, i)
         # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
         try:
             ax.lines.remove(lines[0])
